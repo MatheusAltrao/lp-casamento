0577da12f18b0531993d3f1f541d4706a8c836cb
Matheus Altrao <matheusaltrao2@gmail.com>
2025-11-03 14:18:42 -0400

    feat(footer): Adiciona ícones de coração ao rodapé para melhorar a apresentação da mensagem de amor

--- src/components/sections/footer.tsx
+++ src/components/sections/footer.tsx
@@ -1,9 +1,16 @@
+import { Heart } from "lucide-react";
+
 export default function Footer() {
   return (
     <footer className="py-12 px-4 border-t">
       <div className="max-w-4xl mx-auto text-center space-y-6">
-        <div className="text-xs text-gray-500 pt-4">
-          <p>© {new Date().getFullYear()} - Feito com 💖 para celebrar nosso amor</p>
+        <div className="text-xs text-gray-500 pt-4 flex items-center justify-center gap-2">
+          <p>© 2025 - Feito com</p>
+          <div className="flex ">
+            <Heart fill="#e40397" color="#e40397" />
+            <Heart fill="#ef6d02" color="#ef6d02" />
+          </div>
+          <p>para celebrar nosso amor</p>
         </div>
       </div>
     </footer>
