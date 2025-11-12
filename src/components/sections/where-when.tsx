9943a0cdbe8e09d50f53be2379089eebfaa5d545
Matheus Altrao <matheusaltrao2@gmail.com>
2025-11-12 09:26:52 -0400

    feat(components/where-when): Atualiza informações da cerimônia para `São Francisco Eventos` e ajusta o endereço para `Rod. Marechal Rondon - Agudos, SP, 17120-000`

--- src/components/sections/where-when.tsx
+++ src/components/sections/where-when.tsx
@@ -17,12 +17,12 @@ export function WhereWhen() {
               <div>
                 <h3 className="font-cormorant text-2xl mb-2 text-primary-400">Cerimônia</h3>
                 <p className="text-wedding-text leading-relaxed mb-2">
-                  <strong className="font-cormorant text-primary">Igreja Nossa Senhora da Paz</strong>
+                  <strong className="font-cormorant text-primary">São Francisco Eventos</strong>
                 </p>
                 <p className="text-wedding-text leading-relaxed">
-                  Rua das Flores, 123 - Centro
+                  Rod. Marechal Rondon - Agudos, SP, 17120-000
                   <br />
-                  São Paulo, SP
+                  Bauru, SP
                 </p>
                 <div className="flex items-center gap-2 mt-3 text-primary">
                   <Clock className="w-5 h-5" />
@@ -56,7 +56,7 @@ export function WhereWhen() {
           </div>
 
           <div className="rounded-2xl overflow-hidden shadow-lg h-[400px]">
-            <iframe
+            {/* <iframe
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3657.1976545105!2d-46.65844!3d-23.561414!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMjPCsDMzJzQxLjEiUyA0NsKwMzknMzAuNCJX!5e0!3m2!1sen!2sbr!4v1234567890"
               width="100%"
               height="100%"
@@ -65,6 +65,16 @@ export function WhereWhen() {
               loading="lazy"
               referrerPolicy="no-referrer-when-downgrade"
             />
+ */}
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3687.633930011942!2d-49.028999!3d-22.400345!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94bf5f0b411ff831%3A0x5d76d326cc6ae295!2sS%C3%A3o%20Francisco%20Eventos!5e0!3m2!1spt-BR!2sbr!4v1731423600000!5m2!1spt-BR!2sbr"
+              width="100%"
+              height="100%"
+              style={{ border: 0 }}
+              allowFullScreen
+              loading="lazy"
+              referrerPolicy="no-referrer-when-downgrade"
+            ></iframe>
           </div>
         </div>
       </div>
